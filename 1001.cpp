d04ccf849ff9772c53b7c5ba025d40cc3eb46039
guirocha1804 <48029638+guirocha1804@users.noreply.github.com>
2019-02-27 08:59:07 -0300

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int A,B;
+
+int X;
+
+int main()
+{
+
+ 
+	cin >> A;
+	cin >> B;
+	X = A + B;
+	cout << "X = " << X << endl;
+	system ("pause");
+	return 0;
+}
