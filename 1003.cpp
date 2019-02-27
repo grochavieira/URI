61fa4fa48ccf9361e866deefaf56b8e97b0c8635
guirocha1804 <48029638+guirocha1804@users.noreply.github.com>
2019-02-27 08:58:11 -0300

    Create 1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int A,B,SOMA;
+
+int main()
+{
+
+	cin >> A;
+	cin >> B;
+	SOMA = A + B;
+	
+	cout << "SOMA = " << SOMA << endl;
+
+	system ("pause");
+	return 0;
+}
+
