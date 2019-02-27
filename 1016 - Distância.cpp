529a030504d549a09db1c7775ba6ca0a7e878219
guirocha1804 <48029638+guirocha1804@users.noreply.github.com>
2019-02-27 09:15:52 -0300

    Create 1016 - Distância.cpp

---
+++ 1016 - Distância.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	int Y, KM;
+
+	scanf("%i",&Y);
+	
+	KM = Y * 2;
+	
+    printf("%i minutos\n",KM);
+    
+	system("pause");
+    return 0;
+
+}
