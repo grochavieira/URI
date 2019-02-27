e83ef30a391a44e3fb6e0693870f3b334d02fb8f
guirocha1804 <48029638+guirocha1804@users.noreply.github.com>
2019-02-27 09:00:28 -0300

    Create 1004.cpp

---
+++ 1004.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+
+#include<stdio.h>
+
+using namespace std;
+
+int A,B,PROD;
+
+
+int main()
+{
+
+	cin >> A;
+	cin >> B;
+
+	PROD = A * B;
+
+	cout << "PROD = " << PROD << endl;
+	
+	system("pause");
+	return 0;
+}
