9fad707cb1e5f6cee81fc89f693736c46e8e1f72
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-03-03 10:16:24 -0300

    Create 1067 - Números Ímpares.cpp

---
+++ 1067 - Números Ímpares.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+#include<stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int A, B, X;
+	
+	scanf("%d", &X);
+	
+	for(A = 1; A<=X; A+= 2)
+	{
+		
+	
+	
+		printf("%d\n",A);
+		
+				
+	}
+
+	system("pause");
+	return 0;
+}
