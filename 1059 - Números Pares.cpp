005c1db4adb002ce16de7ff9008f7adcac499c47
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-03-03 10:13:00 -0300

    Create 1059 - Números Pares.cpp

---
+++ 1059 - Números Pares.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include<stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	int i;
+	
+	for(i = 2; i<=100; i = i + 2)
+	
+	
+	
+		printf("%d\n", i);
+		
+		return 0;
+	
+	
+
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
