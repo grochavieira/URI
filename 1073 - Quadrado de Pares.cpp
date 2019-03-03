a3ebda5a5aad0cbcaee34c30f67515a89cd38e5e
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-03-03 10:19:36 -0300

    Create 1073 - Quadrado de Pares.cpp

---
+++ 1073 - Quadrado de Pares.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include<stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int N, X, X1, A;
+	
+	scanf("%d", &N);
+	
+	for(A = 1; A <= N; A+= 1)
+	{
+		
+		if(A%2 == 0)
+		{
+		
+		 X1 = A; X = A*A; printf("%d^2 = %d\n", X1, X);
+		}
+		else
+		{
+		}
+				
+	}
+	
