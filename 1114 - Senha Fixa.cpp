4ca1e5b53c1e9b6f7cd6ff028a5a3ebb690a7143
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-03-03 10:30:42 -0300

    Create 1114 - Senha Fixa.cpp

---
+++ 1114 - Senha Fixa.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdio.h>
+#include<stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<math.h>
+#include <string.h>
+
+using namespace std;
+
+
+
+int main(){
+    
+	int X;
+	
+	while(1)
+	{
+
+		scanf("%d", &X);
+		
+		if(X == 2002)
+		{
+
+			printf("Acesso Permitido\n");
+			break;
+
+		}
+		else
+		{
+
+            printf("Senha Invalida\n");
+
+		}
+
+	}
+	
+	
+	
+
+
+
+	system("pause");
+	return 0;
+}
