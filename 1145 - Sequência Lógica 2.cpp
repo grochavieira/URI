7e560c43b7a6cab5959c9b507d2af9aa7b598a70
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-03-25 12:44:37 -0300

    Create 1145 - Sequência Lógica 2.cpp

---
+++ 1145 - Sequência Lógica 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdio.h>
+#include<stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<math.h>
+#include <string.h>
+
+using namespace std;
+
+
+
+int main(){
+
+	int X,Y,z,a,b=0;
+    scanf("%d%d", &X,&Y);
+    for(a=1; a<=Y; a++)
+    {
+        b++;
+        if(b==X) printf("%d",a);
+        else printf("%d ", a);
+        if(b==X)
+        {
+            b=0;
+            printf("\n");
+        }
+    }
+		
+
+
+
+
+
+	system("pause");
+	return 0;
+}
