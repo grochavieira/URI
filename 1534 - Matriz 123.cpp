513e23cd1d3b992dd6f2d6ba9a64e73a3798f800
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-03-25 14:34:01 -0300

    Create 1534 - Matriz 123.cpp

---
+++ 1534 - Matriz 123.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+#include<stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include<math.h>
+#include <string.h>
+
+using namespace std;
+
+
+
+int main(){
+
+	
+    int z;
+    while(scanf("%d",&z)!=EOF)
+    {
+        int ara[z][z];
+        int a,b,c,d,e,f,g,h;
+        for(a=0; a<z; a++)
+        {
+            for(b=0; b<z; b++)
+                ara[a][b]=3;
+        }
+        for(a=0; a<z; a++)
+            ara[a][a]=1;
+        d=z-1;
+        for(a=0,b=d; a<z;a++,b--)
+            ara[a][b]=2;
+        for(a=0; a<z; a++)
+        {
+            for(b=0; b<z; b++)
+                printf("%d",ara[a][b]);
+            printf("\n");
+        }
+    }
+
+
+
+	system("pause");
+	return 0;
+}
