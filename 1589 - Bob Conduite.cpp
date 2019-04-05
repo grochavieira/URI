1c7d3e056561910be915d9760b60265dd7c0281b
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-04-05 16:34:47 -0300

    Create 1589 - Bob Conduite.cpp

---
+++ 1589 - Bob Conduite.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() {
+  int T, R1, R2, i;
+
+  cin >> T;
+
+  for(int i=0; i<T; i++)
+  {
+    cin >> R1 >> R2;
+    cout << R1 + R2 << endl;
+  }
+
+}
