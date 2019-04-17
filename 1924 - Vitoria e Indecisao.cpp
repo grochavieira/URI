a13dadd6283e123112bba676dbda6fbe164008ba
Guilherme Rocha Vieira <48029638+guirocha1804@users.noreply.github.com>
2019-04-17 08:24:39 -0300

    Create 1924 - Vitoria e Indecisao.cpp

---
+++ 1924 - Vitoria e Indecisao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+using namespace std;
+
+int main() {
+  int casoTeste;
+  string curso;
+  cin >> casoTeste;
+  for(int i=0; i<=casoTeste; i++)
+  {
+    getline(cin, curso);
+  }  
+  cout << "Ciencia da Computacao" << endl;
+}
